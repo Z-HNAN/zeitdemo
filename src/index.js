d6c7d8407fc32b7a5f40f101b59109dd3332e3a6
Z-HNAN <zhn68033@163.com>
2019-11-18 22:10:02 +0800

    init project

---
+++ src/index.js
@@ -0,0 +1,74 @@
+import dva, { connect } from 'dva';
+import { createLogger } from 'redux-logger';
+import { Router, Route, Switch } from 'dva/router';
+
+import styles from './index.less'
+
+const delay = timeout => new Promise(resolve => {
+  setTimeout(resolve, timeout)
+})
+
+// 1. Initialize
+const app = dva({
+  onAction: createLogger()
+});
+
+// 2. Model
+app.model({
+  namespace: 'count',
+  state: {
+    record: 0,
+    current: 0,
+  },
+  reducers: {
+    add(state) {
+      const newCurrent = state.current + 1;
+      return {
+        ...state,
+        record: newCurrent > state.record ? newCurrent : state.record,
+        current: newCurrent,
+      };
+    },
+    minus(state) {
+      return { ...state, current: state.current - 1 };
+    }
+  },
+  effects: {
+    *add (action, { call, put }) {
+      yield call(delay, 1000);
+      yield put({ type: 'minus'})
+    }
+  }
+});
+
+const CountApp = ({count, dispatch }) => {
+  return (
+    <div className={styles.normal}>
+      <div className={styles.record}>Highest Record: {count.record}</div>
+      <div className={styles.current}>{count.current}</div>
+      <div className={styles.button}>
+        <button onClick={() => { dispatch({ type: 'count/add'}) }}>+</button>
+      </div>
+    </div>
+  )
+}
+
+function mapStateToProps(state) {
+  return {
+    count: state.count
+  }
+}
+
+// 3. Router
+const HomePage = connect(mapStateToProps)(CountApp)
+
+app.router(({ history }) =>
+  <Router history={history}>
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+    </Switch>
+  </Router>
+);
+
+// 4. Start
+app.start('#root');
